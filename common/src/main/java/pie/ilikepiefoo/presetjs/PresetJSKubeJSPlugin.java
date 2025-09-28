dd38313872f0f538e0501f47a5f9b652200d1fd2
Hunter19823 <hspragg@asu.edu>
2025-09-27 21:55:47 -0700

    Added a Presets binding.

--- common/src/main/java/pie/ilikepiefoo/presetjs/PresetJSKubeJSPlugin.java
+++ common/src/main/java/pie/ilikepiefoo/presetjs/PresetJSKubeJSPlugin.java
@@ -1,5 +1,13 @@
 package pie.ilikepiefoo.presetjs;
 
 import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingsEvent;
+import pie.ilikepiefoo.presetjs.preset.Presets;
 
-public class PresetJSKubeJSPlugin extends KubeJSPlugin {}
+public class PresetJSKubeJSPlugin extends KubeJSPlugin {
+
+    @Override
+    public void registerBindings(BindingsEvent event) {
+        event.add("Presets", Presets.class);
+    }
+}
---
+++ common/src/main/java/pie/ilikepiefoo/presetjs/preset/Presets.java
@@ -0,0 +1,8 @@
+package pie.ilikepiefoo.presetjs.preset;
+
+import pie.ilikepiefoo.presetjs.preset.block.BlockPresets;
+
+@SuppressWarnings("InstantiationOfUtilityClass")
+public class Presets {
+    public static final BlockPresets block = new BlockPresets();
+}
