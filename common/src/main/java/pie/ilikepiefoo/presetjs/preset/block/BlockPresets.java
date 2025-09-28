02f76cf6c741b3514f8c571e8eea32958c93e460
Hunter19823 <hspragg@asu.edu>
2025-09-27 22:03:26 -0700

    Nested the placement callbacks.

--- common/src/main/java/pie/ilikepiefoo/presetjs/preset/block/BlockPresets.java
+++ common/src/main/java/pie/ilikepiefoo/presetjs/preset/block/BlockPresets.java
@@ -4,36 +4,41 @@ import dev.latvian.mods.kubejs.block.callbacks.BlockStateModifyPlacementCallback
 
 import java.util.function.Consumer;
 
+@SuppressWarnings("InstantiationOfUtilityClass")
 public class BlockPresets {
 
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> FACE_ATTACHED_HORIZONTAL_DIRECTIONAL_BLOCK = BlockStateModifyPlacementCallbackPresets::faceAttachedHorizontalDirectionalBlock;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> FURNACE = BlockStateModifyPlacementCallbackPresets::furnace;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> AMETHYST_CLUSTER = BlockStateModifyPlacementCallbackPresets::amethystCluster;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> SCULK_SENSOR = BlockStateModifyPlacementCallbackPresets::sculkSensor;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> CALIBRATED_SCULK_SENSOR = BlockStateModifyPlacementCallbackPresets::calibratedSculkSensor;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> CAMPFIRE = BlockStateModifyPlacementCallbackPresets::campfire;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> BARREL = BlockStateModifyPlacementCallbackPresets::barrel;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> DISPENSER = BlockStateModifyPlacementCallbackPresets::dispenser;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> DETECTOR_RAIL = BlockStateModifyPlacementCallbackPresets::detectorRail;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> POWERED_RAIL = BlockStateModifyPlacementCallbackPresets::poweredRail;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> RAIL = BlockStateModifyPlacementCallbackPresets::rail;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> CHEST = BlockStateModifyPlacementCallbackPresets::chest;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> DOOR = BlockStateModifyPlacementCallbackPresets::door;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> SLAB = BlockStateModifyPlacementCallbackPresets::slab;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> OBSERVER = BlockStateModifyPlacementCallbackPresets::observer;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> STAIRS = BlockStateModifyPlacementCallbackPresets::stairs;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> TRAP_DOOR = BlockStateModifyPlacementCallbackPresets::trapdoor;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> BED = BlockStateModifyPlacementCallbackPresets::bed;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> BANNER = BlockStateModifyPlacementCallbackPresets::banner;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> LANTERN = BlockStateModifyPlacementCallbackPresets::lantern;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> FENCE = BlockStateModifyPlacementCallbackPresets::fence;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> FENCE_GATE = BlockStateModifyPlacementCallbackPresets::fenceGate;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> WALL = BlockStateModifyPlacementCallbackPresets::wall;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> BAMBOO_STALK = BlockStateModifyPlacementCallbackPresets::bambooStalk;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> BELL = BlockStateModifyPlacementCallbackPresets::bell;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> CANDLE = BlockStateModifyPlacementCallbackPresets::candle;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> CHAIN = BlockStateModifyPlacementCallbackPresets::chain;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> END_ROD = BlockStateModifyPlacementCallbackPresets::endRod;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> HOPPER = BlockStateModifyPlacementCallbackPresets::hopper;
-    public static final Consumer<BlockStateModifyPlacementCallbackJS> NOTE_BLOCK = BlockStateModifyPlacementCallbackPresets::noteBlock;
+    public static PlacementCallbacks placementState = new PlacementCallbacks();
+
+    public static class PlacementCallbacks {
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> FACE_ATTACHED_HORIZONTAL_DIRECTIONAL_BLOCK = BlockStateModifyPlacementCallbackPresets::faceAttachedHorizontalDirectionalBlock;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> FURNACE = BlockStateModifyPlacementCallbackPresets::furnace;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> AMETHYST_CLUSTER = BlockStateModifyPlacementCallbackPresets::amethystCluster;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> SCULK_SENSOR = BlockStateModifyPlacementCallbackPresets::sculkSensor;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> CALIBRATED_SCULK_SENSOR = BlockStateModifyPlacementCallbackPresets::calibratedSculkSensor;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> CAMPFIRE = BlockStateModifyPlacementCallbackPresets::campfire;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> BARREL = BlockStateModifyPlacementCallbackPresets::barrel;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> DISPENSER = BlockStateModifyPlacementCallbackPresets::dispenser;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> DETECTOR_RAIL = BlockStateModifyPlacementCallbackPresets::detectorRail;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> POWERED_RAIL = BlockStateModifyPlacementCallbackPresets::poweredRail;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> RAIL = BlockStateModifyPlacementCallbackPresets::rail;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> CHEST = BlockStateModifyPlacementCallbackPresets::chest;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> DOOR = BlockStateModifyPlacementCallbackPresets::door;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> SLAB = BlockStateModifyPlacementCallbackPresets::slab;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> OBSERVER = BlockStateModifyPlacementCallbackPresets::observer;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> STAIRS = BlockStateModifyPlacementCallbackPresets::stairs;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> TRAP_DOOR = BlockStateModifyPlacementCallbackPresets::trapdoor;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> BED = BlockStateModifyPlacementCallbackPresets::bed;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> BANNER = BlockStateModifyPlacementCallbackPresets::banner;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> LANTERN = BlockStateModifyPlacementCallbackPresets::lantern;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> FENCE = BlockStateModifyPlacementCallbackPresets::fence;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> FENCE_GATE = BlockStateModifyPlacementCallbackPresets::fenceGate;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> WALL = BlockStateModifyPlacementCallbackPresets::wall;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> BAMBOO_STALK = BlockStateModifyPlacementCallbackPresets::bambooStalk;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> BELL = BlockStateModifyPlacementCallbackPresets::bell;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> CANDLE = BlockStateModifyPlacementCallbackPresets::candle;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> CHAIN = BlockStateModifyPlacementCallbackPresets::chain;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> END_ROD = BlockStateModifyPlacementCallbackPresets::endRod;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> HOPPER = BlockStateModifyPlacementCallbackPresets::hopper;
+        public static final Consumer<BlockStateModifyPlacementCallbackJS> NOTE_BLOCK = BlockStateModifyPlacementCallbackPresets::noteBlock;
+    }
 }
